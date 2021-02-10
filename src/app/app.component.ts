7838a438f44ba9e28e710ae429cf32f1a3adebd4
Roman <sanches.roman@gmail.com>
2021-02-10 10:04:43 +0100

    Practica Juego del Ahorcado

---
+++ src/app/app.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'El Ahorcado - Juego';
+
+  palabra= "PROGRAMACION";
+  palabraOculta= "";
+
+  intentos= 0;
+
+  gano = false;
+  perdio= false;
+
+
+
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+  'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+  'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  constructor(){
+    this.palabraOculta = '_ '.repeat(this.palabra.length);
+  }
+
+  comprobar(letra: string){
+
+    this.existeLetra(letra);
+    const palabraOcultaArr = this.palabraOculta.split(" ");
+      for(let i=0; i<this.palabra.length; i++){
+        if (this.palabra[i] === letra){
+          palabraOcultaArr[i] = letra;
+        }
+      }
+      this.palabraOculta = palabraOcultaArr.join(" ");
+      this.verificaGane();
+  }
+
+  verificaGane(){
+
+      const palabraArr = this.palabraOculta.split(" ");
+      const palabraEvaluar = palabraArr.join("");
+
+      if (palabraEvaluar === this.palabra){
+        this.gano = true;
+        console.info("usuario GANO")
+      }
+      if (this.intentos >= 12){
+        this.perdio= true;
+        console.error("Usuario PERDIO")
+      }
+  }
+
+
+
+  existeLetra(letra: string){
+      if (this.palabra.indexOf(letra) >= 0){
+        /* console.log("Existe la letra " + letra); */
+      }
+      else {
+        /* console.warn("letra No existe " + letra); */
+        this.intentos ++
+      }
+
+  }
+
+
+
+}
+
+
